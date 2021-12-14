ee2f5077d2d962c034bb6efbfcc4242e36117c36
Bhon Bunnag <bhonris@gmail.com>
2021-12-14 23:27:08 +0700

    Testing case where test will fail

--- hello-world/tests/unit/test-handler.js
+++ hello-world/tests/unit/test-handler.js
@@ -10,7 +10,7 @@ describe('Tests index', function () {
         const result = await app.lambdaHandler(event, context)
 
         expect(result).to.be.an('object');
-        expect(result.statusCode).to.equal(200);
+        expect(result.statusCode).to.equal(205);
         expect(result.body).to.be.an('string');
 
         let response = JSON.parse(result.body);
